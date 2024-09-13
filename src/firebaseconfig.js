e9dd884afed410fb9c7d617d370cf99d8ebc88f8
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-09-13 16:27:14 -0300

    Actividad guiada

---
+++ src/firebaseconfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBd8h52hRFYT843EzKBshcok4Ca53rhbxI",
+  authDomain: "crud-colaboradores-3c105.firebaseapp.com",
+  projectId: "crud-colaboradores-3c105",
+  storageBucket: "crud-colaboradores-3c105.appspot.com",
+  messagingSenderId: "429080820463",
+  appId: "1:429080820463:web:7973e70111c1017923f91a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
